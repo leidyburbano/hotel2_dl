4125c640a623f5f03a16e828fd2ebf0573b2c583
viviana <ladyburbanop@gmail.com>
2020-12-17 21:18:29 -0500

    nueva actulizacion1

--- app/Http/Controllers/HabitacionesController.php
+++ app/Http/Controllers/HabitacionesController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
+
 class HabitacionesController extends Controller
 {
+
+
     public function showHabitaciones(){
-        $datosh = DB::table('habitacion')
-        ->get();
-        return view('servicios.habitaciones',['Habitaciones'=>$datosh] ); 
+
+        
+        $datosh = DB::table('habitacion as hab')
+        ->join('precio as pre', 'hab.precio_id', '=', 'pre.id')
+
+        ->select('hab.Numero','hab.descripcion','hab.numCamas','hab.foto','pre.precio','pre.Tipo')
+        ->get(); 
+        
+        return view('servicios.habitaciones',['habitacion'=>$datosh]);
     }
-    
 }
--- resources/views/servicios/habitaciones.blade.php
+++ resources/views/servicios/habitaciones.blade.php
@@ -6,16 +6,16 @@
      <h4> Individual - Doble - Triple - Suite - Queen </h4>
      <div class = "container">
       <div class="row row-cols-1 row-cols-md-2 g-4">
-        @foreach($Habitaciones as $h)
+        @foreach($habitacion as $h)
             <div class="col">
                 <div class="card">
                     <img src='{{url("/img/$h->foto")}}' class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Tipos de Habitacion</h5>
-                            <p class="card-text" aling="justify">{{ $h->Numero }}</p>
+                            <p class="card-text" aling="justify">{{ $h->Numero }}: {{$h->Tipo}}</p>
                             <p class="card-text" aling="justify">{{ $h->descripcion }}</p>
                             <p class="card-text" aling="justify"> Numero De Camas: {{ $h->numCamas }}</p>
-                            <p class="card-text" aling="justify"> Precio: {{ $h->precio_id }}</p>
+                            <p class="card-text" aling="justify"> Precio: {{ $h->precio }}</p>
                             
                             <a href="#" class="btn btn-primary">Alquilar Habitacion</a> <br><br>
                             <a href="#" class="btn btn-danger">Devolver Habitacion </a>
